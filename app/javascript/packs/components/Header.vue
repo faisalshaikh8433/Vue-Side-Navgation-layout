39a4f1bfbe567bb3b6a3c0672f7a1fe884c72ae1
faisal <faisalshaikh8433@gmail.com>
2019-12-24 16:22:50 +0530

    adds procfile

--- app/javascript/packs/components/Header.vue
+++ app/javascript/packs/components/Header.vue
@@ -5,7 +5,7 @@
         <p class="text-xl underline">Base Layout (Vue)</p>
       </div>
       <div class="ml-auto text-right">
-        <button @click="$emit('toggleNav')" class="rounded-r hover:bg-red-200">
+        <button @click="$emit('toggleNav')" class="lg:hidden rounded-r hover:bg-red-200">
           <img
             src="https://img.icons8.com/plasticine/100/000000/hamburger.png"
             class="h-10 w-10"
