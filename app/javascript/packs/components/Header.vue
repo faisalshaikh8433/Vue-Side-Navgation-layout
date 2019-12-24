97e2379c9785078040fa53c25bf1f97026a48bea
faisal <faisalshaikh8433@gmail.com>
2019-12-24 16:35:00 +0530

    adds margin to heading in header.vue

--- app/javascript/packs/components/Header.vue
+++ app/javascript/packs/components/Header.vue
@@ -1,15 +1,12 @@
 <template>
   <header>
     <div class="flex justify-center items-center">
-      <div class="w-64 text-center font-bold">
+      <div class="lg:m-3 w-64 text-center font-bold">
         <p class="text-xl underline">Base Layout (Vue)</p>
       </div>
       <div class="ml-auto text-right">
         <button @click="$emit('toggleNav')" class="lg:hidden rounded-r hover:bg-red-200">
-          <img
-            src="https://img.icons8.com/plasticine/100/000000/hamburger.png"
-            class="h-10 w-10"
-          />
+          <img src="https://img.icons8.com/plasticine/100/000000/hamburger.png" class="h-10 w-10" />
         </button>
       </div>
     </div>
