ddccddc09ca9bb6f5a43ec118acd336c89984946
faisal <faisalshaikh8433@gmail.com>
2020-01-02 19:46:36 +0530

    adds response headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  after_filter :cors_set_access_control_headers
+  
+  private
+  def cors_set_access_control_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email'
+    response.headers['Access-Control-Max-Age'] = "1728000"
+  end
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -4,4 +4,10 @@ class ItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  test "should not be save Item without name" do
+    item = Item.new
+    assert_not item.save
+  end
 end
