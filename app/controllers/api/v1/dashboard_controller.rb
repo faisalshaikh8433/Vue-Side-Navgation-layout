7d068315f66513735280490b436d4c9bac920679
faisal <faisalshaikh8433@gmail.com>
2020-01-07 18:37:05 +0530

    adds get url for fetching a file

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -5,4 +5,8 @@ class Api::V1::DashboardController < ApplicationController
     render json: items.as_json(except: [:created_at, :updated_at]), status: :ok
   end
 
+  def files
+    render file: File.join(Rails.root, 'restricter.liquid')
+  end
+
 end
